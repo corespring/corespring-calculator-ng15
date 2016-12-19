2543d3296559fcb03e9b609388887f9b36c0a683
Ben Burton <benjamin.joseph.burton@gmail.com>
2016-12-19 14:09:59 -0500

    Add useful demo content

--- docs/demo/entry.js
+++ docs/demo/entry.js
@@ -7,19 +7,12 @@
         import Controller from 'pie-controller';
         window.pie = window.pie || {};
         window.pie.Controller = Controller;
-
-  import comp1 from 'pie-player';
-  //customElements v1 
-  customElements.define('pie-player', comp1);
-  
-
-  import comp2 from 'pie-control-panel';
-  //customElements v1 
-  customElements.define('pie-control-panel', comp2);
-  
-
-  import comp3 from 'corespring-calculator-ng15';
-  //customElements v1 
-  customElements.define('corespring-calculator-ng15', comp3);
-  
+import PiePlayer from 'pie-player';
+customElements.define('pie-player', PiePlayer);
+import PieControlPanel from 'pie-control-panel';
+customElements.define('pie-control-panel', PieControlPanel);
+import CorespringCalculatorNg15 from 'corespring-calculator-ng15';
+customElements.define('corespring-calculator-ng15', CorespringCalculatorNg15);
+import CorespringChoice from 'corespring-choice';
+customElements.define('corespring-choice', CorespringChoice);
     
